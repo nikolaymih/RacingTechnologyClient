020f79e5ea5fa5d55d918b9209160a2f391ddea4
Nikolay Mihaylov <n.mihaylov@abv.bg>
2021-12-23 13:27:02 +0200

    carousel

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,34 @@
+import { Carousel } from 'antd';
+
+
+const CarouselComponent = () => {
+
+    function onChange() {
+    }
+
+    // const contentStyle = {
+    //     height: '160px',
+    //     color: '#fff',
+    //     lineHeight: '160px',
+    //     textAlign: 'center',
+    //     background: '#364d79',
+    // };
+    return (
+        <Carousel afterChange={onChange}>
+            <div>
+                <h3>1</h3>
+            </div>
+            <div>
+                <h3>2</h3>
+            </div>
+            <div>
+                <h3>3</h3>
+            </div>
+            <div>
+                <h3>4</h3>
+            </div>
+        </Carousel>
+    )
+}
+
+export default CarouselComponent
